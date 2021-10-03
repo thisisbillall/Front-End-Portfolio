17e7566547b61781c5a454f22a89662036125f4c
Mohammad Bilal <mdbilal7620@gmail.com>
2021-10-03 17:54:00 +0530

    project cards

--- src/App.js
+++ src/App.js
@@ -1,8 +1,47 @@
 import './App.css';
-import stars from './images/stars.png';
 import Card from './components/card';
-function App() {
+import flapy  from "./images/flapy.png";
+import pixSearch  from "./images/pixSearch.jpg";
+import recipo  from "./images/recipo.jpg";
+import quizzy  from "./images/quizzy.jpg";
+import flutterLogo from "./images/flutterLogo.png";
+import reactLogo from "./images/reactLogo.png";
 
+function App() {
+  const projectData =[
+   {
+    name:"Flappy X",
+    img:flapy,
+    demo:"ffff",
+    source:"fffff",
+    about:"An extended Flappy Bird game with attractive UI , can track scores ,level.To be published on Play/App stores.",
+    tech:flutterLogo,
+  },
+   {
+    name:"pixSearch",
+    img:pixSearch,
+    demo:"",
+    source:"",
+    about:"An Image Search and download app like Unsplash developed using REACTJS and PIXABAY API hosted on Netlify.",
+    tech:reactLogo,
+   },
+   {
+    name:"Recipo",
+    img:recipo,
+    demo:"",
+    source:"",
+    about:"A static app to help the Food lovers and noob cooks to get the recipes of their favorite dishes in hand.",
+    tech:flutterLogo,
+   },
+   {
+    name:"Quizzy",
+    img:quizzy,
+    demo:"",
+    source:"",
+    about:"A simple UI based app intended to create awareness about COVID among peoplely. It can run on all the platforms using single codebase.",
+    tech:flutterLogo,
+   },
+  ];
   return (
     <>
     <div className="row">
@@ -13,16 +52,29 @@ function App() {
         <nav>
           <ul>
             <li>Home</li>
-            <li>Projects</li>
+            <a href="#projects"><li>Projects</li></a>
+            <li>Skills</li>
             <li>Socials</li>
-            <li>Others</li>
+            <li>More</li>
           </ul>
         </nav>
       </header>
     </div>
     <section id="projects">
-        <div class = "card-container" >
-          <Card/>
+    <strong className="sec-heading">PROJECTS</strong>
+        <div class = "card-container">
+        <div className="card">
+          <Card  data={projectData[0]}/>
+        </div>
+        <div className="card">
+          <Card data={projectData[1]}/>
+        </div>
+        <div className="card">
+          <Card data={projectData[2]}/>
+        </div>
+        <div className="card">
+          <Card data={projectData[3]}/>
+        </div>
         </div>
       </section>
     </>
--- src/components/card.js
+++ src/components/card.js
@@ -1,17 +1,40 @@
 import React from "react";
+import { useRef } from 'react';
+import Flippy, { FrontSide, BackSide } from 'react-flippy';
+import './card.css';
+// import flapy  from "../images/flapy.png";
 
-const Card = () => {
-    const cardCss = {
-        color:'white',
-        width:'15rem',
-        height:'15rem',
-        borderRadius:'2rem',
-        backgroundColor:'black',
-      };
+const Card = ({data}) => {
+  const ref = useRef();
     return (  
-        <div className="card" style={cardCss}>
-            <br/><p>vskjvkjnvkdnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn</p>
+     <div style={{ width: '200px', height: '240px',backgroundColor: 'pink', borderRadius:"1rem"}} >
+              <Flippy 
+                  flipOnHover={true} // default false
+                  flipOnClick={true} // default false
+                  flipDirection="horizontal" // horizontal or vertical
+                  ref={ref} // to use toggle method like ref.curret.toggle()
+                  // if you pass isFlipped prop component will be controlled component.
+                  // and other props, which will go to div
+                  style={{ width: '200px', height: '200px' }} /// these are optional style, it is not necessary
+        >
+          <FrontSide style={{backgroundColor: 'seashell', borderRadius:"1rem",overflow:"hidden"}} >
+                  <strong>{data.name}</strong>
+                  <img className="main-img" src={data.img}/>
+          </FrontSide>
+          <BackSide style={{ backgroundColor: 'seashell',borderRadius:"0rem",overflow:"hidden"}}>
+                <strong>About</strong>
+                <p>{data.about}</p>
+            <div className="tech-stack">
+                  <h6 className="tech-headings">Tech-stack used:</h6>
+                  <img className="tech-logo" src={data.tech} />
+            </div>
+          </BackSide>
+        </Flippy>
+        <div className="bottom-sec">
+          <a href={data.demo}>Demo</a>
+          <a href={data.source}>Source</a>
         </div>
+    </div>
     );
 }
  
