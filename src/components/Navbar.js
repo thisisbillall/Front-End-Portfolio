3872b3ef79a2c219a72e4ee7e3fd377f1c00b493
Mohammad Bilal <mdbilal7620@gmail.com>
2021-10-17 19:54:44 +0530

    Responsive Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import React from "react";
+import '../styles/navbar.css';
+
+const Navbar = () => {
+    return ( 
+        <nav>
+            <div className="logo">
+                <strong>LOGO</strong>
+            </div>
+            <i class="far fa-bars"></i>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#skills">Skills</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+     );
+}
+ 
+export default Navbar;
+<div>
+
+</div>
